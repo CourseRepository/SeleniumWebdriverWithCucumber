72f6cdaa3a6b7471b38ab3d1e3425a1c14a0ef7f
rathr1 <RahulSingh.Rathore@in.pega.com>
2018-03-04 18:07:15 +0530

    Multiple window

--- src/main/java/com/webdriver/iframes/Iframes.feature
+++ src/main/java/com/webdriver/iframes/Iframes.feature
@@ -8,3 +8,10 @@ Scenario: Click on the element presetn in Iframe
 	When IFrame_I perform the click on the element presetn in iframe, then it should click
 	And IFrame_I perform the click action on the home button
 	When IFrame_I call the quit method the browser will close
+	
+Scenario: Working with MultipleWindow 
+	Given MultipleWindow_I open the web page "https://www.w3schools.com/js/js_popup.asp"
+	Then MultipleWindow_I click on button and it will open the child browser window
+	And MultipleWindow_I switch to child browser window and click on home button
+	Then MultipleWindow_I click on the element present in parent window
+	When IFrame_I call the quit method the browser will close
--- src/main/java/com/webdriver/iframes/IframesStepDfn.java
+++ src/main/java/com/webdriver/iframes/IframesStepDfn.java
@@ -1,7 +1,7 @@
 package com.webdriver.iframes;
 
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
@@ -9,16 +9,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.picocontainer.converters.NewInstanceConverter;
 
 import com.webdriver.browser.CustomChromeDriver;
 import com.webdriver.helper.BrowserHelper;
 import com.webdriver.helper.ButtonHelper;
-import com.webdriver.helper.TextBoxHelper;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import gherkin.lexer.Th;
 
 public class IframesStepDfn {
 	
@@ -27,6 +27,7 @@ public class IframesStepDfn {
 	private BrowserHelper browserHelper;
 	private ButtonHelper buttonHelper;
 	private WebElement element;
+	private ArrayList<String> windowId;
 	
 	@Given("^IFrame_I navigate to the webpage \"([^\"]*)\"$")
 	public void iframe_i_navigate_to_the_webpage(String page) throws Throwable {
@@ -57,13 +58,52 @@ public class IframesStepDfn {
 		driver.switchTo().defaultContent(); // switch to parent DOM -> doc Obj model
 	    buttonHelper.click(By.id("tryhome"));
 	}
-
 	
+	@Given("^MultipleWindow_I open the web page \"([^\"]*)\"$")
+	public void multiplewindow_i_open_the_web_page(String arg1) throws Throwable {
+	    driver.get(arg1);
+	    WebDriverWait wait = getWait(driver, 60);
+	    wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#main .w3-example:nth-child(13) [target]")));
+	}
+
+	@Then("^MultipleWindow_I click on button and it will open the child browser window$")
+	public void multiplewindow_i_click_on_button_and_it_will_open_the_child_browser_window() throws Throwable {
+		buttonHelper.click(By.cssSelector("#main .w3-example:nth-child(13) [target]"));
+		WebDriverWait wait = getWait(driver, 60);
+		wait.until(ExpectedConditions.numberOfWindowsToBe(2));
+		/*try {
+			Thread.sleep(3000);
+		} catch (Exception e) {
+			// Ignore
+		}*/
+		windowId = new ArrayList<>(driver.getWindowHandles());
+		driver.switchTo().window(windowId.get(1));
+	}
+
+	@Then("^MultipleWindow_I switch to child browser window and click on home button$")
+	public void multiplewindow_i_switch_to_child_browser_window_and_click_on_home_button() throws Throwable {
+		WebDriverWait wait = getWait(driver, 60);
+	    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("tryhome")));
+	    buttonHelper.click(By.id("tryhome"));
+	    wait.until(ExpectedConditions.numberOfWindowsToBe(3));
+	    windowId = new ArrayList<>(driver.getWindowHandles());
+		driver.switchTo().window(windowId.get(2));
+	}
+	
+	@Then("^MultipleWindow_I click on the element present in parent window$")
+	public void multiplewindow_i_click_on_the_element_present_in_parent_window() throws Throwable {
+		driver.switchTo().window(windowId.get(0));
+		buttonHelper.click(By.cssSelector("#main .w3-example:nth-child(13) [target]"));
+		driver.switchTo().window(windowId.get(2));
+	}
+
 	
 	@When("^IFrame_I call the quit method the browser will close$")
 	public void iframe_i_call_the_quit_method_the_browser_will_close() throws Throwable {
 	    if(driver != null){
-	    	driver.quit();
+	    	
+	    	driver.quit(); // it will close all the window and stop the web driver
+	    	driver.close(); //it will close the window, currently pointed by driver instance
 	    }
 	}
 	
