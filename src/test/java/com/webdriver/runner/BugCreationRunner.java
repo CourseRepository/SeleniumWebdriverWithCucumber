c90ceccd68113f5e6ae36e9b95b06336148b79e9
rathr1 <RahulSingh.Rathore@in.pega.com>
2018-05-20 16:46:50 +0530

    Added the parallel runner

---
+++ src/test/java/com/webdriver/runner/BugCreationRunner.java
@@ -0,0 +1,21 @@
+package com.webdriver.runner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(
+		features = {"src/main/java/com/webdriver/page/BugCreation.feature"},
+		glue = {"com.webdriver.page","com.webdriver.generichook"},
+		dryRun = false,
+		monochrome = true,
+		plugin = {"pretty","junit:target/junitReport/junitreport.xml"}
+		)
+public class BugCreationRunner extends AbstractTestNGCucumberTests {
+
+}
+
+/**
+ * 1. To create a generic method that will navigate to corr page
+ * 2. Use Factory design patter to get the instance of corr page after the navigation
+ * */
+ 
---
+++ src/test/java/com/webdriver/runner/WebdriverWaitFunctionRunner.java
@@ -0,0 +1,15 @@
+package com.webdriver.runner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(
+		features = {"src/main/java/com/webdriver/wait/WebdriverWaitFunction.feature"},
+		glue = {"com.webdriver.wait"},
+		dryRun = false,
+		monochrome = true,
+		plugin = {"pretty"}
+		)
+public class WebdriverWaitFunctionRunner extends AbstractTestNGCucumberTests {
+
+}
