e1bafc0f058a1ea9d681af8c0f8d215a843bbe36
rathr1 <RahulSingh.Rathore@in.pega.com>
2018-06-02 12:58:05 +0530

    Grid

--- src/main/java/com/webdriver/browser/CustomInternetExplorerDriver.java
+++ src/main/java/com/webdriver/browser/CustomInternetExplorerDriver.java
@@ -1,10 +1,17 @@
 package com.webdriver.browser;
 
+import static org.openqa.selenium.ie.InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.ie.InternetExplorerDriverService;
 import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.openqa.selenium.internal.ElementScrollBehavior;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class CustomInternetExplorerDriver implements BrowserConfiguration  {
 	
@@ -22,6 +29,28 @@ public class CustomInternetExplorerDriver implements BrowserConfiguration  {
 	}
 	
 	public WebDriver getInternetExplorerDriver(){
+		InternetExplorerDriver driver = getNormalDriver();
+		return driver;
+	}
+	
+	private WebDriver getGridDriver(){
+		DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+		capabilities.setAcceptInsecureCerts(true);
+		capabilities.setJavascriptEnabled(true);
+		capabilities.setCapability(InternetExplorerDriver.IE_ENSURE_CLEAN_SESSION, true);
+		capabilities.setCapability(InternetExplorerDriver.IGNORE_ZOOM_SETTING, true);
+		capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+		capabilities.setCapability(InternetExplorerDriver.ELEMENT_SCROLL_BEHAVIOR, ElementScrollBehavior.BOTTOM);
+		WebDriver driver = null;
+		try {
+			driver = new RemoteWebDriver(getHubUrl(), capabilities);
+		} catch (MalformedURLException e) {
+			throw new RuntimeException(e.getMessage());
+		}
+		return driver;
+	}
+	
+	private InternetExplorerDriver getNormalDriver() {
 		setDriverExecutable();
 		InternetExplorerOptions options = getInternetExplorerOption();
 		InternetExplorerDriver driver = new InternetExplorerDriver(options);
@@ -30,7 +59,32 @@ public class CustomInternetExplorerDriver implements BrowserConfiguration  {
 
 	@Override
 	public WebDriver getBrowserDriver() {
-		return getInternetExplorerDriver();
+		if(isGridSetup())
+			return getGridDriver();
+		else
+			return getNormalDriver();
+	}
+
+	@Override
+	public boolean isGridSetup() {
+		String property = System.getProperty("IsGridSetup");
+		
+		if(null == property || property.isEmpty())
+			throw new RuntimeException("Invalid Property Value");
+		
+		if("false".equalsIgnoreCase(property))
+			return false;
+		return true;
+	}
+
+	@Override
+	public URL getHubUrl() throws MalformedURLException {
+		String property = System.getProperty("HubUrl");
+		
+		if(null == property || property.isEmpty())
+			throw new RuntimeException("Invalid Hub url");
+		
+		return new URL(property);
 	}
 
 	
