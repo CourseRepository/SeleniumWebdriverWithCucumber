103abf225aa79a9bcfd8203fcf70544a988c6e72
rathr1 <RahulSingh.Rathore@in.pega.com>
2018-06-02 13:23:27 +0530

    Grid setup Runner

---
+++ src/test/java/com/webdriver/runner/WebelementFunctionRunner.java
@@ -0,0 +1,14 @@
+package com.webdriver.runner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(
+		features = {"src/main/java/com/webdriver/webelementfunction/WebelementFunction.feature"},
+		glue = {"com.webdriver.webelementfunction","com.webdriver.generichook"},
+		dryRun = false,
+		monochrome = true
+		)
+public class WebelementFunctionRunner extends AbstractTestNGCucumberTests {
+
+}
